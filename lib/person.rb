0d2cf6afce660420687706851e16ed40beea3052
Loise <loise.kimani@student.moringaschool.com>
2023-02-13 01:50:04 +0300

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -21,38 +21,5 @@ class Person
     @happiness
   end
 
-  def happiness=(amount)
-    amount = 10 if amount > 10
-    amount = 0 if amount < 0
-    @happiness = amount
-  end
-
-  def hygiene
-    @hygiene
-  end
-
-  def hygiene=(amount)
-    amount = 10 if amount > 10
-    amount = 0 if amount < 0
-    @hygiene = amount
-  end
-
-  def happy?
-    @happiness > 7
-  end
-
-  def clean?
-    @hygiene > 7
-  end
-
-  def get_paid(amount)
-    @bank_account += amount
-    "all about the benjamins"
-  end
-
-  def take_bath
-    self.hygiene = [@hygiene + 4, 10].min
-    "♪ Rub-a-dub just relaxing in the tub ♪"
-  end
   end
 end
