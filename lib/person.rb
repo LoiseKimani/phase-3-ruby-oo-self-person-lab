ef94b5d16b499b3efe48bcccc97aa492f8a0c86b
Loise <loise.kimani@student.moringaschool.com>
2023-02-14 07:23:05 +0300

    Work

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,3 @@
-# your code goes here
 class Person
   attr_reader :name
   
@@ -21,5 +20,64 @@ class Person
     @happiness
   end
 
+  def happiness=(amount)
+    amount = 10 if amount > 10
+    amount = 0 if amount < 0
+    @happiness = amount
+  end
+
+  def hygiene
+    @hygiene
+  end
+
+  def hygiene=(amount)
+    amount = 10 if amount > 10
+    amount = 0 if amount < 0
+    @hygiene = amount
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(amount)
+    @bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene = [@hygiene + 4, 10].min
+    "♪ Rub-a-dub just relaxing in the tub ♪"
+  end
+
+  def work_out
+    self.hygiene = [@hygiene - 3, 0].max
+    self.happiness = [@happiness + 2, 10].min
+    "♪ Another one bites the dust ♪"
+  end
+
+  def call_friend(friend)
+    self.happiness = [@happiness + 3, 10].min
+    friend.happiness = [friend.happiness + 3, 10].min
+    "Hi #{friend.name}! It's great to hear from you!"
+  end
+
+  def start_conversation(friend, topic)
+    case topic
+    when "politics"
+      self.happiness = [@happiness - 2, 0].max
+      friend.happiness = [friend.happiness - 2, 0].max
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      self.happiness = [@happiness + 1, 10].min
+      friend.happiness = [friend.happiness + 1, 10].min
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
   end
 end
